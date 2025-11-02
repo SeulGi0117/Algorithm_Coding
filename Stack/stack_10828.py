5a4fb37af44cb83a1d494bbeb3b76705e9bbb264
Seulgi <jusanha17@gmail.com>
2025-11-02 20:38:04 +0900

    Initial commit from local

---
+++ Stack/stack_10828.py
@@ -0,0 +1,30 @@
+# 2025-10-29 코테 스터디 1주차 백준 10828 스택 문제
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    cmd = input().split()
+
+    if cmd[0] == 'push':
+        stack.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
