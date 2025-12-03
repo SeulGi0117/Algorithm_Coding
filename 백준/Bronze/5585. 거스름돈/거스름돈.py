f06bdf4633aa29ab2ddd595f4be9d9a0c002b743
Seulgi <117719441+SeulGi0117@users.noreply.github.com>
2025-12-03 18:47:41 +0900

    [Bronze II] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+changes = 1000 - n
+won = [500, 100, 50, 10, 5, 1]
+result = 0
+
+for w in won:
+    cnt = changes// w
+    result += cnt
+    changes%=w
+print(result)
