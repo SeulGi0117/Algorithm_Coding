fb96be26e3403303f17d081e6e27beae67a78bbb
Seulgi <117719441+SeulGi0117@users.noreply.github.com>
2025-12-11 19:51:44 +0900

    [Silver V] Title: 지뢰찾기, Time: 408 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/4108. 지뢰찾기/지뢰찾기.py
+++ 백준/Silver/4108. 지뢰찾기/지뢰찾기.py
@@ -2,44 +2,39 @@ import sys
 
 input = sys.stdin.readline
 
-# 8방향 이동 (상, 하, 좌, 우, 대각선 4개)
-dr = [-1, -1, -1, 0, 0, 1, 1, 1]
-dc = [-1, 0, 1, -1, 1, -1, 0, 1]
+# 상하좌우 대각선 이동방향
+# k번째 방향으로 얼마나 이동하는지. range 8돌면 차례대로 모든 방향 검사 가능
+direction_r = [-1, -1, -1, 0, 0, 1, 1, 1]
+direction_c = [-1, 0, 1, -1, 1, -1, 0, 1]
 
 while True:
+    # 입력받기
     R, C = map(int, input().split())
     if R == 0 and C == 0:
-        break  # 입력 종료
+        break
 
-    # 지뢰밭 입력 받기
-    board = [list(input().strip()) for _ in range(R)]
+    nemo = [list(input().strip()) for _ in range(R)]
 
-    # 결과를 담을 배열 (문자 하나씩 채울 예정)
     result = [[''] * C for _ in range(R)]
 
     for r in range(R):
         for c in range(C):
-            # 지뢰면 그대로 '*'
-            if board[r][c] == '*':
+            if nemo[r][c] == '*':
                 result[r][c] = '*'
                 continue
 
-            # 지뢰가 아니면 주변 8칸의 지뢰 개수 세기
-            mine_count = 0
+            # 지뢰 아닐때
+            cnt = 0
             for k in range(8):
-                nr = dr[k] + r
-                nc = dc[k] + c
+                nr = direction_r[k] + r
+                nc = direction_c[k] + c
 
-                # 범위 밖이면 건너뜀
                 if nr < 0 or nr >= R or nc < 0 or nc >= C:
                     continue
 
-                if board[nr][nc] == '*':
-                    mine_count += 1
+                if nemo[nr][nc] == '*':
+                    cnt += 1
+            result[r][c] = str(cnt)
 
-            # 숫자로 채우기 (문자형으로 저장)
-            result[r][c] = str(mine_count)
-
-    # 출력 (각 줄을 문자열로 합쳐서 출력)
     for r in range(R):
         print(''.join(result[r]))
